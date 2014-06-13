be46c9d7ac28d9ae8bdc39dc9374e0138e7dbe01
dlandy1 <DanLandy@Daniels-MacBook-Pro-6.local>
2014-06-13 17:33:55 -0400

    fizz

---
+++ fizz.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	var fizz = "<p>fizz</p>";
+	var buzz = "<p>buzz</p>";
+	var fizzbuzz= "<p>fizzbuzz</p>";
+	var sent = "<p> </p>"
+	var number= prompt("Put in a number");
+	var error = true;
+	while (error) {
+	if (number >0 ==false || number%1 !== 0) {
+		number= prompt("You must put in a number and it must be greater than zero, put in a number");
+	}
+	else{
+		error = false;
+	}
+	};
+	for (var i = 1; i <= number; i++) {
+		if(i%3 == 0 && i%5 == 0){
+			$("body").append(fizzbuzz); 
+		}
+		else if (i%5 == 0) {
+			$("body").append(buzz); 
+		}
+		else if (i%3 == 0) {
+			$("body").append(fizz); 
+		}
+		else{
+		$("body").append(sent).append(i);
+	};
+	};
+})
